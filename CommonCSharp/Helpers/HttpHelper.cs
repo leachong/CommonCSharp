855ab5db9a483b84804f1f2125e2b42eb8df1612
DESKTOP-ED5EBLM\lichong <leachong@qq.com>
2019-10-13 12:10:44 +0800

    调整http

--- CommonCSharp/Helpers/HttpHelper.cs
+++ CommonCSharp/Helpers/HttpHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommonCSharp.Models;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -10,30 +11,48 @@ namespace CommonCSharp.Helpers
 {
     public class HttpHelper
     {
-        public static string PostMulti(string url, Dictionary<string, string> param, byte[] body, int len, string cookies = "")
+        //文件数据模板
+        static string fileFormdataTemplate =
+            "\r\n--{2}" +
+            "\r\nContent-Disposition: form-data; name=\"{0}\"; filename=\"{1}\"" +
+            //"\r\nContent-Type: application/octet-stream" +
+            "\r\n\r\n";
+        //文本数据模板
+        static string dataFormdataTemplate =
+            "\r\n--{2}" +
+            "\r\nContent-Disposition: form-data; name=\"{0}\"" +
+            "\r\n\r\n{1}";
+
+        public static string PostFile(string url, Dictionary<string, string> param, string fileKey, byte[] fileData)
         {
-            var postDataStr = convertDictionary(param);
-            postDataStr += "&content=";
-
-            if (body.Length != len)
+            List<FormItemModel> lstForm = new List<FormItemModel>();
+            foreach (var key in param.Keys)
             {
-                var temp = new byte[len];
-                Array.Copy(body, temp, len);
-                body = temp;
+                lstForm.Add(new FormItemModel()
+                {
+                    Key = key,
+                    Value = param[key]
+                });
             }
-
-            return Post(url, Encoding.UTF8.GetBytes(postDataStr), body);
+            lstForm.Add(new FormItemModel()
+            {
+                Key = fileKey,
+                Value = "",
+                FileName = fileKey,
+                FileBytes = fileData
+            });
+            return PostForm(url, lstForm);
         }
         static string convertDictionary(Dictionary<string, string> param)
         {
             StringBuilder sb = new StringBuilder();
             foreach (var key in param.Keys)
             {
-                sb.Append(key + "=" + param[key] + "&");
+                sb.Append(WebUtility.UrlEncode(key) + "=" + WebUtility.UrlEncode(param[key]) + "&");
             }
             sb.Remove(sb.Length - 1, 1);
 
-            return WebUtility.UrlEncode(sb.ToString());
+            return sb.ToString();
         }
         public static string Post(string url, Dictionary<string, string> param)
         {
@@ -45,27 +64,20 @@ namespace CommonCSharp.Helpers
             return Post(url, Encoding.UTF8.GetBytes(postDataStr));
         }
 
-        public static string Post(string url, params byte[][] postData)
+        public static string Post(string url, byte[] postData)
         {
-            var bytesLength = postData?.Sum((bs) => bs.Length);
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             request.Method = "POST";
             //if (!string.IsNullOrEmpty(cookies))
             //    request.Headers.Add("Cookie", cookies);
             request.ContentType = "application/x-www-form-urlencoded;charset=utf-8";
-            request.ContentLength = bytesLength ?? 0;
+            request.ContentLength = postData.Length;
 
             if (postData != null)
             {
-                using (BinaryWriter writer = new BinaryWriter(request.GetRequestStream(), Encoding.UTF8))
+                using (var s = request.GetRequestStream())
                 {
-                    foreach (var data in postData)
-                    {
-                        if (data != null)
-                        {
-                            writer.Write(data);
-                        }
-                    }
+                    s.Write(postData, 0, postData.Length);
                 }
             }
             using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
@@ -82,7 +94,6 @@ namespace CommonCSharp.Helpers
                 }
             }
         }
-
         public static string Get(string Url, string cookies = "")
         {
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
@@ -104,5 +115,106 @@ namespace CommonCSharp.Helpers
                 }
             }
         }
+        //2.提交表单主逻辑实现
+        /// <summary>
+        /// 使用Post方法获取字符串结果
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="formItems">Post表单内容</param>
+        /// <param name="cookieContainer"></param>
+        /// <param name="timeOut">默认20秒</param>
+        /// <param name="encoding">响应内容的编码类型（默认utf-8）</param>
+        /// <returns></returns>
+        public static string PostForm(string url, List<FormItemModel> formItems, CookieContainer cookieContainer = null, string refererUrl = null, Encoding encoding = null, int timeOut = 20000)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            #region 初始化请求对象
+            request.Method = "POST";
+            request.Timeout = timeOut;
+            request.Accept = "*/*";
+            request.KeepAlive = true;
+            request.UserAgent = "python-requests/2.22.0";
+            request.Headers.Add("Accept-Encoding", "gzip, deflate");
+            //request.Connection = "keep-alive";
+            request.Expect = "";
+            //request.Headers.Add("Connection", "keep-alive");
+            //request.Headers.Remove("Expect");
+            if (!string.IsNullOrEmpty(refererUrl))
+                request.Referer = refererUrl;
+            if (cookieContainer != null)
+                request.CookieContainer = cookieContainer;
+            #endregion
+
+            string boundary = "----" + DateTime.Now.Ticks.ToString("x");//分隔符
+            request.ContentType = string.Format("multipart/form-data; boundary={0}", boundary);
+
+            var lstData = GetFormData(formItems, boundary);
+            //请求流
+            request.ContentLength = lstData.Sum((arr) => arr.Length);
+
+            //直接写入流
+            using (Stream requestStream = request.GetRequestStream())
+            {
+                foreach (var bytes in lstData)
+                {
+                    requestStream.Write(bytes, 0, bytes.Length);
+                }
+            }
+            
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            if (cookieContainer != null)
+            {
+                response.Cookies = cookieContainer.GetCookies(response.ResponseUri);
+            }
+
+            using (Stream responseStream = response.GetResponseStream())
+            {
+                using (StreamReader myStreamReader = new StreamReader(responseStream, encoding ?? Encoding.UTF8))
+                {
+                    string retString = myStreamReader.ReadToEnd();
+                    return retString;
+                }
+            }
+        }
+
+        static List<byte[]> GetFormData(List<FormItemModel> formItems, string boundary)
+        {
+            List<byte[]> lstData = new List<byte[]>();
+            bool firstLine = true;
+            foreach (var item in formItems)
+            {
+                string formdata = null;
+                if (item.IsFile)
+                {
+                    //上传文件
+                    formdata = string.Format(fileFormdataTemplate, item.Key, item.FileName, boundary);
+                }
+                else
+                {
+                    //上传文本
+                    formdata = string.Format(dataFormdataTemplate, item.Key, item.Value, boundary);
+                }
+
+                //统一处理
+                byte[] formdataBytes = null;
+                //第一行不需要换行
+                if (firstLine)
+                {
+                    formdataBytes = Encoding.UTF8.GetBytes(formdata.Substring(2, formdata.Length - 2));
+                    firstLine = false;
+                }
+                else
+                {
+                    formdataBytes = Encoding.UTF8.GetBytes(formdata);
+                }
+                lstData.Add(formdataBytes);
+                if (item.FileBytes != null && item.FileBytes.Length > 0)
+                {
+                    lstData.Add(item.FileBytes);
+                }
+            }
+            return lstData;
+        }
     }
 }
+
---
+++ CommonCSharp/Models/FormItemModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonCSharp.Models
+{
+    //1.表示一个表单项的对象
+    /// <summary>
+    /// 表单数据项
+    /// </summary>
+    public class FormItemModel
+    {
+        /// <summary>
+        /// 表单键，request["key"]
+        /// </summary>
+        public string Key { set; get; }
+        /// <summary>
+        /// 表单值,上传文件时忽略，request["key"].value
+        /// </summary>
+        public string Value { set; get; }
+        /// <summary>
+        /// 是否是文件
+        /// </summary>
+        public bool IsFile
+        {
+            get
+            {
+                if ((FileStream == null || FileStream.Length <= 0) && (FileBytes == null || FileBytes.Length <= 0))
+                    return false;
+
+                if (string.IsNullOrWhiteSpace(FileName))
+                    throw new Exception("上传文件时 FileName 属性值不能为空");
+                return true;
+            }
+        }
+        /// <summary>
+        /// 上传的文件名
+        /// </summary>
+        public string FileName { set; get; }
+        /// <summary>
+        /// 上传的文件内容
+        /// </summary>
+        public Stream FileStream { set; get; }
+
+        public byte[] FileBytes { get; set; }
+    }
+}
